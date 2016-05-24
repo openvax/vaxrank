0b33a096f9bf614680a11aba9236053dd61f50b7
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-05-24 18:58:36 -0400

    WIP: in the middle of rearranging

--- script/vaxrank.py
+++ script/vaxrank.py
@@ -14,101 +14,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Script to generate vaccine peptide predictions from somatic cancer variants,
+patient HLA type, and tumor RNA-seq data.
+
+Example usage:
+    vaxrank
+        --vcf somatic.vcf \
+        --bam rnaseq.bam \
+        --min-vaccine-peptide-length 23 \
+        --max-vaccine-peptide-length 27 \
+        --output-csv vaccine-peptides.csv
+"""
+
+
 from __future__ import print_function, division, absolute_import
 import logging
-import argparse
 
-import varcode
-from pysam import AlignmentFile
-
-from isovar.protein_sequence import variants_to_protein_sequences_dataframe
-from isovar.default_parameters import (
-    MIN_READS_SUPPORTING_VARIANT_CDNA_SEQUENCE,
-    MIN_TRANSCRIPT_PREFIX_LENGTH,
-    MAX_REFERENCE_TRANSCRIPT_MISMATCHES,
-    PROTEIN_SEQUENCE_LEGNTH,
-    MAX_PROTEIN_SEQUENCES_PER_VARIANT,
-)
-
-from isovar import args
-
-args.extend_parser_with_necessary_args(parser)
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument(
-    "--vcf",
-    required=True,
-    help="VCF file containing somatic variants")
-
-parser.add_argument(
-    "--bam",
-    required=True,
-    help="BAM file containing RNAseq reads")
-
-parser.add_argument(
-    "--genome",
-    default=None)
-
-parser.add_argument(
-    "--min-reads",
-    type=int,
-    default=MIN_READS_SUPPORTING_VARIANT_CDNA_SEQUENCE)
-
-parser.add_argument(
-    "--protein-sequence-length",
-    default=PROTEIN_SEQUENCE_LEGNTH,
-    type=int)
-
-parser.add_argument(
-    "--max-sequences-per-variant",
-    type=int,
-    default=MAX_PROTEIN_SEQUENCES_PER_VARIANT)
-
-parser.add_argument(
-    "--max-reference-transcript-mismatches",
-    type=int,
-    default=MAX_REFERENCE_TRANSCRIPT_MISMATCHES)
-
-parser.add_argument(
-    "--min-transcript-prefix-length",
-    type=int,
-    default=MIN_TRANSCRIPT_PREFIX_LENGTH,
-    help=(
-        "Number of nucleotides before the variant we try to match against "
-        "a reference transcript. Values greater than zero exclude variants "
-        "near the start codon of transcripts without 5' UTRs."))
-
-parser.add_argument(
-    "--min-mapping-quality",
-    type=int,
-    default=0,
-    help="Minimum MAPQ value to allow for a read")
-
-parser.add_argument(
-    "--output",
-    default="isovar-translate-variants-results.csv",
-    help="Name of CSV file which contains predicted sequences")
+from vaxrank.args import arg_parser
+from vaxrank.vaccine_peptides import vaccine_peptides_dataframe_from_args
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    args = parser.parse_args()
-
+    args = arg_parser.parse_args()
     print(args)
 
-    variants = varcode.load_vcf(
-        args.vcf,
-        genome=args.genome)
-
-    samfile = AlignmentFile(args.bam)
-    df = variants_to_protein_sequences_dataframe(
-        variants=variants,
-        samfile=samfile,
-        protein_sequence_length=args.protein_sequence_length,
-        min_reads_supporting_rna_sequence=args.min_reads,
-        min_transcript_prefix_length=args.min_transcript_prefix_length,
-        max_transcript_mismatches=args.max_reference_transcript_mismatches,
-        max_protein_sequences_per_variant=args.max_sequences_per_variant,
-        min_mapping_quality=args.min_mapping_quality)
-    print(df)
-    df.to_csv(args.output)
+    df_vaccine_peptides = vaccine_peptides_dataframe_from_args(args)
+    print(df_vaccine_peptides)
+    if args.output_csv:
+        df_vaccine_peptides.to_csv(args.output_csv)
---
+++ vaxrank/epitope_scoring.py
@@ -0,0 +1,118 @@
+import numpy as np
+
+class EpitopeScorer(object):
+    def __init__(
+            self,
+            ic50_cutoff=500.0,
+            transformation_function=None,
+            allele_weights=None):
+        """
+        Parameters
+        ----------
+        ic50_cutoff : float
+            Value above which we ignore epitopes
+
+        transformation_function : fn : float -> float (optional)
+            Given the predicted binding quantity, transform it into a value
+            between 0.0 and 1.0
+
+        allele_weights : dict (optional)
+            Associates each MHC allele name with an importance weight
+        """
+        self.ic50_cutoff = ic50_cutoff
+        self.transformation_function = transformation_function
+        self.allele_weights = allele_weights
+
+    def allele_weight(self, allele_name):
+        if self.allele_weights:
+            assert allele_name in self.allele_weights, \
+                "Allele not in weight dictionary: %s" % (allele_name,)
+            return self.allele_weights[allele_name]
+        else:
+            return 1.0
+
+    def binding_value_score(self, ic50, allele_name=None):
+        """
+        Transform a binding prediction for a particular allele into a
+        rescaled score (non-binders get a score of 0.0, strongest
+        predicted binders should have a score of 1.0)
+
+        Parameters
+        ----------
+        ic50 : float
+
+        allele_name : string (optional)
+        """
+        if self.transformation_function:
+            score = self.transformation_function(ic50)
+        else:
+            score = 1.0 if ic50 < self.ic50_cutoff else 0.0
+        allele_weight = self.allele_weight(allele_name) if allele_name else 1.0
+        return allele_weight * score
+
+    def binding_record_score(self, record):
+        return self.binding_value_score(
+            ic50=record["IC50"],
+            allele_name=record["allele"])
+
+    def sum_binding_record_scores(self, epitope_binding_prediction_records):
+        """
+
+        Parameters
+        ----------
+
+        epitope_binding_prediction_records : dictionary of binding predictions
+            Keys are alleles, values are binding prediction records with
+            fields such as 'MHC_IC50' and 'MHC_Percentile_Rank'
+
+        Returns sum of normalized binding prediction scores.
+        """
+        if isinstance(epitope_binding_prediction_records, dict):
+            epitope_binding_prediction_records = list(
+                epitope_binding_prediction_records.values())
+
+        return sum(
+            self.binding_record_score(record)
+            for record
+            in epitope_binding_prediction_records
+        )
+
+    def epitope_score(self, epitope):
+        binding_records = epitope['MHC_Allele_Scores']
+        return self.sum_binding_record_scores(binding_records)
+
+
+class DecreasingLogisticFunction(object):
+    def __init__(self, midpoint, width):
+        assert width > 0
+        self.midpoint = float(midpoint)
+        self.width = float(width)
+
+    def __call__(self, value):
+        rescaled = (float(value) - self.midpoint) / self.width
+        # simplification of 1.0 - logistic(x) = logistic(-x)
+        logistic = 1.0 / (1.0 + np.exp(rescaled))
+
+        # since we're scoring IC50 values, let's normalize the output
+        # so IC50 near 0.0 always returns a score of 1.0
+        normalizer = 1.0 / (1.0 + np.exp(-self.midpoint / self.width))
+
+        return logistic / normalizer
+
+# add up all the epitopes with IC50 <= 500nM
+simple_class1_ic50_epitope_scorer = EpitopeScorer(cutoff=500.0)
+
+# default midpoint and width for logistic determined by max likelihood fit
+# for data from Alessandro Sette's 1994 paper:
+#
+#   "The relationship between class I binding affinity
+#    and immunogenicity of potential cytotoxic T cell epitopes.
+#
+# TODO: Use a large dataset to find MHC binding range predicted to #
+# correlate with immunogenicity
+logistic_fn = DecreasingLogisticFunction(midpoint=350.0, width=150.0)
+
+logistic_class1_ic50_epitope_scorer = EpitopeScorer(
+    cutoff=2000.0,
+    transformation_function=logistic_fn,
+)
