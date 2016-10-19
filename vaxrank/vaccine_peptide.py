1dffbf07380c1006486d39bac20ed191d687bf23
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-10-19 00:57:38 -0400

    added manufacturability to sorting logic

--- vaxrank/vaccine_peptide.py
+++ vaxrank/vaccine_peptide.py
@@ -19,12 +19,15 @@ from collections import namedtuple
 
 import numpy as np
 
+from .manufacturability import compute_manufacturability_scores
+
 VaccinePeptideBase = namedtuple(
     "VaccinePeptide", [
         "mutant_protein_fragment",
         "epitope_predictions",
         "mutant_epitope_score",
-        "wildtype_epitope_score"])
+        "wildtype_epitope_score",
+        "manufacturability_scores"])
 
 class VaccinePeptide(VaccinePeptideBase):
     """
@@ -40,12 +43,15 @@ class VaccinePeptide(VaccinePeptideBase):
             p.logistic_score()
             for p in epitope_predictions
             if p.overlaps_mutation)
+
         return VaccinePeptideBase.__new__(
             cls,
             mutant_protein_fragment=mutant_protein_fragment,
             epitope_predictions=epitope_predictions,
             mutant_epitope_score=mutant_epitope_score,
-            wildtype_epitope_score=wildtype_epitope_score)
+            wildtype_epitope_score=wildtype_epitope_score,
+            manufacturability_scores=compute_manufacturability_scores(
+                mutant_protein_fragment.amino_acids))
 
     def lexicographic_sort_key(self):
         """
@@ -54,13 +60,34 @@ class VaccinePeptide(VaccinePeptideBase):
         score positive (since we want fewer wildtype epitopes) but the others
         negative (since we want more of them).
         """
-        return (
+        # since we're sorting in decreasing order, numbers which we want
+        # to be larger must have their signs flipped
+        essential_score_tuple = (
+            # Sum of normalized MHC binding affinities of subsequences
             -self.mutant_epitope_score,
-            -self.mutant_protein_fragment.n_alt_reads,
+            # Number of reads supporting the variant
+            -self.mutant_protein_fragment.n_alt_reads
+        )
+        manufacturability_score_tuple = tuple(self.manufacturability_scores)
+        extra_score_tuple = (
+            # Number of reads supporting the particular protein sequence
+            # sequence we're using for this vaccine peptide. Currently
+            # all vaccine peptides are drawn from the same larger sequence
+            # so this score shouldn't change.
             -self.mutant_protein_fragment.n_alt_reads_supporting_protein_sequence,
+            # Minimize the sum of non-mutant MHC binding scores
             self.wildtype_epitope_score,
+            # All else being equal, we prefer to maximize the number of
+            # mutant amino acids
             -self.mutant_protein_fragment.n_mutant_amino_acids,
+            # If nothing else can serve as a tie break then try to center
+            # the mutation in the vaccine peptide.
             -self.mutant_protein_fragment.mutation_distance_from_edge
+        )
+        return (
+            essential_score_tuple +
+            manufacturability_score_tuple +
+            extra_score_tuple
         )
 
     @property
