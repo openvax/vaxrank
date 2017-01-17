ef92450a28275ef25c45747dfbc42adbab6378ce
Julia K <julia326@gmail.com>
2017-01-17 17:11:58 -0500

    Up the isovar version to that which supports include-mismatches-after-variant.

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
             'numpy',
             'pandas',
             'varcode>=0.5.9',
-            'isovar>=0.5.0',
+            'isovar>=0.6.0',
             'mhctools>=0.3.1',
             'roman',
             'jinja2',
