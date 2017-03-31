c48d1551563faced2376458dc57b424fb1d5fedb
Julia K <julia326@gmail.com>
2017-03-31 16:05:48 -0400

    Explicitly specifying epitope lengths we want to check in vaxrank test script.

--- run-vaxrank-b16-test-data.sh
+++ run-vaxrank-b16-test-data.sh
@@ -4,6 +4,7 @@ vaxrank \
     --vaccine-peptide-length 25 \
     --mhc-predictor netmhc \
     --mhc-alleles H2-Kb,H2-Db \
+    --mhc-epitope-lengths 8 \
     --padding-around-mutation 5 \
     --output-ascii-report vaccine-peptides-report.txt \
     --output-html-report vaccine-peptides-report.html \
