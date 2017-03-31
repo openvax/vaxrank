fb1a6bf3ccfd4fa575fac5e7578ca162c78b2e90
Julia K <julia326@gmail.com>
2017-03-31 19:29:55 -0400

    Trying netmhcpan

--- test/test_shell_script.py
+++ test/test_shell_script.py
@@ -21,7 +21,7 @@ cli_args_for_b16_seqdata_real_predictor = [
     "--vcf", data_path("b16.f10/b16.vcf"),
     "--bam", data_path("b16.f10/b16.combined.bam"),
     "--vaccine-peptide-length", "25",
-    "--mhc-predictor", "netmhc",
+    "--mhc-predictor", "netmhcpan",
     "--mhc-alleles", "H2-Kb,H2-Db",
     "--mhc-epitope-lengths", "8",
     "--padding-around-mutation", "5",
