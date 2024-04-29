cbe632ed9d75efeda873801f335fb9f785cc2e71
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-04-29 12:49:48 -0400

    read install reqs from requirements.txt

---
+++ vaxrank/config.py
@@ -0,0 +1,11 @@
+import msgspec
+
+class Confiug(msgspec.Struct):
+
+    """Parameters for score, filtering, and ranking both epitopes and vaccine peptides"""
+    logistic_epitope_score_midpoint : float =350.0
+    logistic_epitope_score_width : float = 150.0
+    
+    min_epitope_score : float = 0.0
+    binding_affinity_cutoff : float = 5000.0
+    
