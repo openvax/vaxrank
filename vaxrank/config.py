2522bbe0d964b770995d8c538196d202632f127f
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-05-10 16:09:45 -0400

    add CLI to config

--- vaxrank/config.py
+++ vaxrank/config.py
@@ -1,4 +1,20 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import msgspec
+
+MIN_EPITOPE_SCORE_DEFAULT = 1e-10
+BINDING_AFFINITY_CUTOFF = 5000.0
 
 class Config(msgspec.Struct):
 
@@ -6,6 +22,6 @@ class Config(msgspec.Struct):
     logistic_epitope_score_midpoint : float = 350.0
     logistic_epitope_score_width : float = 150.0
     
-    min_epitope_score : float = 0.0
-    binding_affinity_cutoff : float = 5000.0
+    min_epitope_score : float = MIN_EPITOPE_SCORE_DEFAULT
+    binding_affinity_cutoff : float = BINDING_AFFINITY_CUTOFF
     
